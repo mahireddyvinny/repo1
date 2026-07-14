15cbf9e6cc466f43cf6dedc0f01f06ff040f10f0
mahireddyvinny <vinaymahireddy@gmail.com>
2026-07-14 22:28:24 +0530

    Update testdevbranch.py

--- testdevbranch.py
+++ testdevbranch.py
@@ -1,1 +1,8 @@
-dfjadskljfadsjkfjasdj
+# Basic calculator for addition, subtraction, multiplication, division
+def calculator(a, b):
+    print("Addition:", a + b)
+    print("Subtraction:", a - b)
+    print("Multiplication:", a * b)
+    print("Division:", a / b)
+
+calculator(10, 5)
